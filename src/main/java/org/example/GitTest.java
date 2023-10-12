9db5e8fa6acd6cba9099feadc909c214d5e14ae1
jix <jix6548@gmail.com>
2023-10-12 18:49:21 +0800

    修改

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("pull test2 hello world!");
+        System.out.println("代理lll");
     }
 }
